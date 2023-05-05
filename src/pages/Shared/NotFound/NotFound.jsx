29943cdce5a5ffd3de68297a02eda11737ca5a2c
jhtanjim~ <jh.tanjim.4@gmail.com>
2023-05-05 19:45:48 +0600

    data

--- src/pages/Shared/NotFound/NotFound.jsx
+++ src/pages/Shared/NotFound/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div className="not-found text-center">
-      <img className='w-25' src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?w=2000" alt="404" />
+      <img className='w-25' src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-1932.jpg?size=626&ext=jpg&ga=GA1.2.2105197794.1682091372&semt=ais" alt="404" />
     </div>
   );
 };
